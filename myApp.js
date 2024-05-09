e089a1fbba30b78791f40a02e029f2f519ec35f3
Mac Clark <clark.mac99@gmail.com>
2024-05-09 14:34:58 -0400

    Initial commit

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
 let express = require('express');
 let app = express();
 
+absolutePath = __dirname + '/views/index.html';
 
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
